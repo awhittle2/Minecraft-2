d48da66358bb343b3d98100d9770199643fb371b
Abi (She/her) <91515654+awhittle2@users.noreply.github.com>
2023-06-06 16:32:08 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,132 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "minecraft_sg" {
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Minecraft-2"
+  }
+}
+
+resource "aws_key_pair" "home" {
+  key_name   = "Home"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "minecraft_2" {
+  ami           = "ami-03f65b8614a860c29"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
+  associate_public_ip_address = false
+  key_name = aws_key_pair.home.key_name
+  user_data = <<-EOF
+  #!/bin/bash
+
+  # Update system
+  sudo apt-get update
+
+  # Install dependencies
+  sudo apt-get install ca-certificates curl gnupg
+
+  # Setup Docker repository
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  echo "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  # Update system again
+  sudo apt-get update
+
+  # Install Docker Engine
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  # Install Docker Compose
+  sudo apt-get install docker-compose
+
+  # Setup Minecraft directory
+  mkdir minecraft_directory
+  cd minecraft_directory
+
+  # Create docker-compose.yml file
+  cat << DOCKER_COMPOSE > docker-compose.yml
+  version: "3"
+
+  services:
+    mc:
+      image: itzg/minecraft-server:java17-alpine
+      ports:
+          - 25565:25565
+      environment:
+          EULA: "TRUE"
+      tty: true
+      stdin_open: true
+      restart: unless-stopped
+      volumes:
+          - ./minecraft-data:/data
+  DOCKER_COMPOSE
+  # Start the server
+  docker-compose up -d
+  
+  # Create systemd service
+  sudo bash -c 'cat > /etc/systemd/system/minecraft.service << SYSTEMD_SERVICE
+  [Unit]
+  Description=Minecraft Server
+  After=docker.service
+  Requires=docker.service
+
+  [Service]
+  WorkingDirectory=/home/ubuntu/minecraft_directory
+  ExecStart=/usr/bin/docker-compose up
+  ExecStop=/usr/bin/docker-compose down
+  Restart=always
+  User=ubuntu
+  Group=docker
+
+  [Install]
+  WantedBy=multi-user.target
+  SYSTEMD_SERVICE'
+  
+  # Wait for 5 minutes
+  sleep 300
+
+  # Enable and start the service
+  sudo systemctl enable minecraft
+  sudo systemctl start minecraft
+EOF
+  tags = {
+    Name = "Minecraft-2"
+  }
+}
+
+resource "aws_eip" "minecraft_eip" {
+  instance = aws_instance.minecraft_2.id
+  vpc      = true
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.minecraft_2.public_ip
+}
