f5ba8217649773fa54ae821bb0b35cdbeae9e718
Abi (She/her) <91515654+awhittle2@users.noreply.github.com>
2023-06-06 19:35:53 -0700

    Update service-setup.sh

--- service-setup.sh
+++ service-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Note this file is not used by terraform, but the code inside it is
+
 sudo bash -c 'cat > /etc/systemd/system/minecraft.service << SYSTEMD_SERVICE
   [Unit]
   Description=Minecraft Server
@@ -18,8 +20,8 @@ sudo bash -c 'cat > /etc/systemd/system/minecraft.service << SYSTEMD_SERVICE
   WantedBy=multi-user.target
   SYSTEMD_SERVICE'
   
-  # Wait for 5 minutes
-  sleep 300
+  # Wait for 3.5 minutes, to give the docker container enough time to set up
+  sleep 210
 
   # Enable and start the service
   sudo systemctl enable minecraft
